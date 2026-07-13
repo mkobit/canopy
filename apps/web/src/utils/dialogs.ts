d4a9392cb6f3ecbc2299b5cd657c3c56319fa627
Mike Kobit <mkobit@gmail.com>
2026-07-13 09:06:07 -0500

    chore: fix functional/prefer-tacit lint warnings

--- apps/web/src/utils/dialogs.ts
+++ apps/web/src/utils/dialogs.ts
@@ -3,11 +3,13 @@ export const showAlert = (message?: string): void => {
   alert(message);
 };
 
+// eslint-disable-next-line functional/prefer-tacit -- Wrapper to restrict parameters and avoid global namespace reference issues
 export const showPrompt = (message?: string, _default?: string): string | null => {
   // eslint-disable-next-line no-restricted-globals -- Wrapper for global prompt
   return prompt(message, _default);
 };
 
+// eslint-disable-next-line functional/prefer-tacit -- Wrapper to restrict parameters and avoid global namespace reference issues
 export const showConfirm = (message?: string): boolean => {
   return confirm(message);
 };
