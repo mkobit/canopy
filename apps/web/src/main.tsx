508a7631af8294f777a6811f1345691adff6f343
Mike Kobit <mkobit@gmail.com>
2026-07-13 00:35:53 -0500

    feature: self-host custom fonts for offline support (#359)
    * build: install @fontsource packages for custom fonts
    
    * style: add fontsource imports for self-hosting fonts
    
    * style: import fontsource stylesheets in main.tsx to fix build bundling
    
    * html: remove google fonts preconnect and external stylesheet link tags

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/jetbrains-mono/400.css';
+import '@fontsource/jetbrains-mono/500.css';
+import '@fontsource/jetbrains-mono/700.css';
+import '@fontsource/space-grotesk/400.css';
+import '@fontsource/space-grotesk/500.css';
+import '@fontsource/space-grotesk/600.css';
+import '@fontsource/space-grotesk/700.css';
 import './index.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- #root is guaranteed by index.html
