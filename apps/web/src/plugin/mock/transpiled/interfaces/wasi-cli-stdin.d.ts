f153108ee0080534f356104c29c34e9f7b7fa6d2
Mike Kobit <mkobit@gmail.com>
2026-08-09 10:52:49 -0500

    build: untrack generated plugin artifacts and gitignore them (canopy-fav) (#430)

--- apps/web/src/plugin/mock/transpiled/interfaces/wasi-cli-stdin.d.ts
+++
@@ -1,3 +0,0 @@
-/** @module Interface wasi:cli/stdin@0.2.10 **/
-export function getStdin(): InputStream;
-export type InputStream = import('./wasi-io-streams.js').InputStream;
--- apps/web/src/plugin/types/interfaces/canopy-graph-plugin-lifecycle.d.ts
+++
@@ -1,5 +0,0 @@
-/** @module Interface canopy:graph/plugin-lifecycle **/
-export function getManifest(): PluginManifest;
-export function initialize(): void;
-export function shutdown(): void;
-export type PluginManifest = import('./canopy-graph-plugin-manifest.js').PluginManifest;
